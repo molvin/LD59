9e11e78fc7caee2d769335b8532d5f7dd26475e6
molvin <akerstrom.perry@gmail.com>
2026-04-19 12:12:38 +0200

    pickups progress

---
+++ LD59/Assets/Scripts/World/Pickupable.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Pickupable : Interactable
+{
+    protected override void Interact(Transform interactorTransform)
+    {
+        // TODO: lock player movement, put object infront of camera
+
+    }
+}
