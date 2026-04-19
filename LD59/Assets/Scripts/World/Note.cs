29e046247aaae921e91cf795763f16eaa9767e78
molvin <akerstrom.perry@gmail.com>
2026-04-19 21:27:56 +0200

    some fixes

--- LD59/Assets/Scripts/World/Note.cs
+++ LD59/Assets/Scripts/World/Note.cs
@@ -3,17 +3,21 @@ using UnityEngine;
 
 public class Note : Pickupable
 {
-    public string Text;
+    public string TextAssetName;
     public TextMeshProUGUI CanvasText;
+
+    private string text;
 
     private void Start()
     {
-        CanvasText.text = Text;
+        TextAsset txt = Resources.Load<TextAsset>($"Text/{TextAssetName}");
+        CanvasText.text = txt.text;
+        text = txt.text;
     }
 
     protected override void Drop()
     {
-        FindFirstObjectByType<PolaroidBook>().AddNote(Text);
+        FindFirstObjectByType<PolaroidBook>().AddNote(text);
         Destroy(gameObject);
     }
 }
