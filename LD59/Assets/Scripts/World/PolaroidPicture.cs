cee44a4b49b6a9de66979e2d5e693ee9167aa315
molvin <akerstrom.perry@gmail.com>
2026-04-19 21:06:24 +0200

    fixes, notes progress, fixed shader warning

--- LD59/Assets/Scripts/World/PolaroidPicture.cs
+++ LD59/Assets/Scripts/World/PolaroidPicture.cs
@@ -1,14 +1,19 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PolaroidPicture : Pickupable
 {
     public Texture2D Picture;
+    public string Text;
+
     public Image CanvasImage;
+    public TextMeshProUGUI CanvasText;
 
     private void Start()
     {
         CanvasImage.sprite = Sprite.Create(Picture, new Rect(0, 0, Picture.width, Picture.height), new Vector2(0.5f, 0.5f));
+        CanvasText.text = Text;
     }
 
     protected override void Drop()
